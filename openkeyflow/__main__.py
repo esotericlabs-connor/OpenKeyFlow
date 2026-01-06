fb0b7bea1901b16332c7e259afc28b348a27fce6
bwetzel1 <openkeyflow@connormail.slmail.me>
2026-01-05 19:55:02 -0800

    ..

--- openkeyflow/__main__.py
+++ openkeyflow/__main__.py
@@ -1,6 +1,6 @@
 """Module entry point for OpenKeyFlow."""
 
-from .launcher import main
+from openkeyflow.launcher import main
 
 
 if __name__ == "__main__":  # pragma: no cover - module execution entry point
